9740f5405ae060121ae12c2e3bc84b5e86cf4aba
HectorStiven <132477892+HectorStiven@users.noreply.github.com>
2023-06-22 16:57:31 -0500

    aceses

--- src/img/principal.tsx
+++ src/img/principal.tsx
@@ -2,7 +2,7 @@ export default function ImagenMegafiestasMenu() {
 
     return (
         // <img src='../images/inicio.jpeg  ' style={{ height: "300px" }} />
-        <h3>hola bebegit</h3>
+        <h3>hola bebe</h3>
     );
 
 }

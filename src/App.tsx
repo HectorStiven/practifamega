73c08c58c813e3d7d9b327e08fefc51628c2b7a1
HectorStiven <132477892+HectorStiven@users.noreply.github.com>
2023-06-22 16:48:29 -0500

    abc

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import ResponsiveDrawer from './components/drawer/drawer';
-
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 // import RutasMegafiestas from './routes/rutas';
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
 import { BrowserRouter } from 'react-router-dom';
 
+const rootElement = document.getElementById('root');
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
 );

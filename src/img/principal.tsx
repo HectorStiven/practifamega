567982e873d483120e54c3405116e24d896d4f0c
HectorStiven <132477892+HectorStiven@users.noreply.github.com>
2023-06-22 16:54:04 -0500

    probar1

--- src/img/principal.tsx
+++ src/img/principal.tsx
@@ -1,7 +1,8 @@
 export default function ImagenMegafiestasMenu() {
 
     return (
-        <img src='../images/inicio.jpeg  ' style={{ height: "300px" }} />
+        // <img src='../images/inicio.jpeg  ' style={{ height: "300px" }} />
+        <h3>hola bebegit</h3>
     );
 
 }

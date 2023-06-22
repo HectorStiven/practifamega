e4f8a352fbb99878bcc6bfe8d187f26f0aa89179
HectorStiven <132477892+HectorStiven@users.noreply.github.com>
2023-06-22 17:05:26 -0500

    comit imagen prueba

--- src/img/principal.tsx
+++ src/img/principal.tsx
@@ -1,8 +1,8 @@
 export default function ImagenMegafiestasMenu() {
 
     return (
-        // <img src='../images/inicio.jpeg  ' style={{ height: "300px" }} />
-        <h3>hola bebe</h3>
+        <img src='../images/inicio.jpeg  ' style={{ height: "300px" }} alt="Imagen de inicio" />
+        
     );
 
 }

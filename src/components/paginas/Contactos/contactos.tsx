41ad670dbeaa32e62aa4b14c1770783f963623e6
miguel-73 <miguelangelmurciaocampo73@gmail.com>
2023-06-25 10:33:56 -0500

    creacioon de lista de contactar

--- src/components/paginas/Contactos/contactos.tsx
+++ src/components/paginas/Contactos/contactos.tsx
@@ -1,8 +1,42 @@
 
+import React, { useState, useEffect } from 'react';
+import { Tree } from 'primereact/tree';
+import { TreeNode } from 'primereact/treenode';
+import { NodeService } from './NodeService';
+import { Grid } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
 export default function ContactosMegafietas() {
+    const [nodes, setNodes] = useState<TreeNode[]>([]);
 
+    useEffect(() => {
+        NodeService.getTreeNodes().then((data) => setNodes(data));
+    }, []);
+
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#1A2027',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
     return (
-       <h1>hola concontactp</h1>
-    );
+        <>
+            <Grid container spacing={3}>
+                <Grid item xs={6} >
+                    <div className="card flex justify-content-center">
+                        <Tree value={nodes} className="w-full md:w-30rem" />
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <Item>xs=6</Item>
+                </Grid>
+                <Grid item xs={12}>
+                    <Item>xs</Item>
+                </Grid>
+            </Grid>
+        </>
 
+    )
 }

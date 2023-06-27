3acf3195858b9a188da51b9442fd77663beb638c
HectorStiven <132477892+HectorStiven@users.noreply.github.com>
2023-06-27 16:45:02 -0500

    mejora de drawer y mejora contacto

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -1,59 +1,48 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
 import RutasMegafiestas from '../../routes/rutas';
-import { Grid } from '@mui/material';
 
 const drawerWidth = 240;
 
-interface Props {
-  window?: () => Window;
-}
+const links = ['Inicio', 'Paquetes', 'Servicios', 'Info General', 'Contacto'];
 
-export default function ResponsiveDrawer(props: Props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+const ResponsiveDrawer = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
-  const links = ['Inicio', 'Paquetes', 'Servicios', 'Info General', 'Contacto'];
-
-  const drawer = (
+  const renderDrawer = () => (
     <div>
       <Toolbar />
       <Divider />
       <List>
         {links.map((text, index) => (
           <Link key={index} to={`/${text.toLowerCase()}`} style={{ textDecoration: 'none' }}>
-            <ListItem key={text} disablePadding>
+            <ListItem disablePadding>
               <ListItemButton>
                 <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText
-                  primary={text.toUpperCase()}
-                  style={{
-                    color: 'black',
-                    border: '0px solid black',
-                    margin: -10,
-                    width: 'auto',
-                  }}
-                />
+                <ListItemText primary={text.toUpperCase()} />
               </ListItemButton>
             </ListItem>
           </Link>
@@ -63,34 +52,39 @@ export default function ResponsiveDrawer(props: Props) {
     </div>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const renderAppBar = () => (
+    <AppBar position="fixed" sx={{ backgroundColor: '#ffdc00', height: 64 }}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          MEGAFIESTAS
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+
+  const renderGrid = () => (
+    <Grid container spacing={0} justifyContent="center">
+      <Grid item xs={12} sm={10} md={8}>
+        <RutasMegafiestas />
+      </Grid>
+    </Grid>
+  );
 
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar style={{ backgroundColor: '#ffdc00' }} position="fixed" sx={{ height: 64 }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            MEGAFIESTASsss
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
+      {renderAppBar()}
+      <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
         <Drawer
-          container={container}
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
@@ -102,7 +96,7 @@ export default function ResponsiveDrawer(props: Props) {
             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
           }}
         >
-          {drawer}
+          {renderDrawer()}
         </Drawer>
         <Drawer
           variant="permanent"
@@ -112,26 +106,15 @@ export default function ResponsiveDrawer(props: Props) {
           }}
           open
         >
-          {drawer}
+          {renderDrawer()}
         </Drawer>
       </Box>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          height: '100%',
-          overflow: 'auto',
-          marginLeft: { sm: drawerWidth },
-        }}
-      >
+      <Box component="main" sx={{ flexGrow: 1, p: 3, height: '100%', overflow: 'auto' }}>
         <Toolbar />
-        <Grid container spacing={2} justifyContent="center">
-          <Grid item xs={12} sm={10} md={8}>
-            <RutasMegafiestas />
-          </Grid>
-        </Grid>
+        {renderGrid()}
       </Box>
     </Box>
   );
-}
+};
+
+export default ResponsiveDrawer;

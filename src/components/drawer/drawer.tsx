31ca92aa567084518811f2a0db266b5dbba5e6f3
HectorStiven <132477892+HectorStiven@users.noreply.github.com>
2023-06-27 18:02:33 -0500

    mejora de drawer y mejora contacto

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -20,7 +20,9 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import RutasMegafiestas from '../../routes/rutas';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
 
+import PhoneIphoneTwoToneIcon from '@mui/icons-material/PhoneIphoneTwoTone';
 const drawerWidth = 240;
 
 const links = ['Inicio', 'Paquetes', 'Servicios', 'Info General', 'Contacto'];
@@ -31,41 +33,46 @@ const ResponsiveDrawer = () => {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+  const icons = [InboxIcon, MailIcon, VideoLibraryIcon, PhoneIphoneTwoToneIcon]; // Array de iconos disponibles
 
   const renderDrawer = () => (
     <div>
       <Toolbar />
       <Divider />
       <List>
-        {links.map((text, index) => (
-          <Link key={index} to={`/${text.toLowerCase()}`} style={{ textDecoration: 'none' }}>
-            <ListItem
-              style={{
-                width:'96%',
-                color: 'black', // Cambia el color del texto a negro
-                background: " radial-gradient(circle, rgba(255,0,0,1) 0%, rgba(255,220,0,1) 83%)",
-                padding: '1px', // Añade un espacio de relleno de 1px alrededor del ListItem
-                borderRadius: 7, // Agrega bordes redondeados al ListItem
-                boxShadow: '0 2px 4px 2px rgba(0, 0, 0, 0.2)', // Agrega una sombra más intensa al ListItem
-                fontSize: '28px', // Cambia el tamaño de fuente a 28px
-                fontWeight: 'bold', // Aplica un estilo de fuente en negrita al texto
-                textAlign: 'center', // Centra el texto dentro del ListItem
-                textTransform: 'uppercase', // Transforma el texto a mayúsculas
-                margin: '5px', // Agrega un margen de 5px alrededor del ListItem
-                lineHeight: '1.5', // Ajusta el espacio entre líneas del texto
-                cursor: 'pointer', // Cambia el cursor al pasar sobre el ListItem
-                transition: 'background-color 0.3s ease', // Agrega una transición suave al cambiar el color de fondo
-              }}
-              disablePadding
-            >
-
-              <ListItemButton>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text.toUpperCase()} />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-        ))}
+        {links.map((text, index) => {
+          const Icono = icons[index % icons.length]; // Selecciona el icono correspondiente según el índice
+          return (
+            <Link key={index} to={`/${text.toLowerCase()}`} style={{ textDecoration: 'none' }}>
+              <ListItem
+                style={{
+                  width: '95%',
+                  color: 'black',
+                  background: 'radial-gradient(circle, rgba(255,0,0,1) 0%, rgba(255,220,0,1) 83%)',
+                  padding: '1px',
+                  borderRadius: 7,
+                  boxShadow: '0 2px 4px 2px rgba(0, 0, 0, 0.2)',
+                  fontSize: '28px',
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                  textTransform: 'uppercase',
+                  margin: '6px',
+                  lineHeight: '1.5',
+                  cursor: 'pointer',
+                  transition: 'background-color 0.3s ease',
+                }}
+                disablePadding
+              >
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Icono />
+                  </ListItemIcon>
+                  <ListItemText primary={text.toUpperCase()} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          );
+        })}
       </List>
       <Divider />
     </div>
